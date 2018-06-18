a87ada288c6a21ff02eb4a53622ba52e0453f08c
Andrew Locke <alocke12992@gmail.com>
2018-06-18 08:51:02 -0600

    Adds remove from cart feature

--- app/javascript/packs/Store/index.js
+++ app/javascript/packs/Store/index.js
@@ -19,7 +19,12 @@ class Store extends React.Component {
   }
 
   removeFromCart = (sku) => {
-
+    let cart = this.state.cart
+    if (sku.id in cart.items && cart.items[sku.id] > 0) {
+      cart.items[sku.id] = cart.items[sku.id] - 1
+      cart.total -= sku.price
+      this.setState({cart})
+    }
   }
 
   render() {
