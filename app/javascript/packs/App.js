52912a6cf0b34cf8fd66bd469ea63a50ad291a77
Andrew Locke <alocke12992@gmail.com>
2018-06-17 19:03:43 -0600

    Adds root react app

---
+++ app/javascript/packs/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Store from './Store';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Store />
+      </div>
+    )
+  }
+}
+
+export default App
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<App />, document.getElementById('store'))
+})
